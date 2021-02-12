6e880d9c8652c6410e04a12397dd42ae9e6f361f
Joao <Joao@ASROCK-J5005>
2021-02-12 11:48:07 +0000

    Versão inicial.

---
+++ src/projetocolab/Main.java
@@ -0,0 +1,37 @@
+// --- PSI M17B - projeto para trabalho autónomo, 12 e 15 de fevereiro
+// O que é necessário fazer (página 5 do PDF das aulas 151 e 152):
+// - aceitar o convite para colaborar no repositório deste projeto.
+// - clonar o repositório com o NetBeans.
+// - fazer alterações ao projeto no NetBeans, de acordo com as indicações dadas
+//   no próprio projeto - VER main().
+
+// Depois:
+// - atualizar o repositório local – Team | Commit...
+// - enviar o código para o repositório remoto – Team | Remote | Push…
+package projetocolab;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Retangulo[] poligonos = new Retangulo[20];
+        
+        poligonos[0] = new Retangulo("a00000", 4, 3);
+        System.out.println(poligonos[0]);
+        
+        // O que deve ser acrescentado ao código:
+        // - na prática, repetir o código das linhas 18 e 19, mas instanciando
+        //   um novo elemento do array, sem ser o de índice 0;
+        // - no primeiro atributo, relativo ao nome, deve ser colocado o
+        //   vosso «nome de utilizador» da conta da escola (a00000 no exemplo);
+        // - para os atributos da largura e altura devem usar quaisquer valores
+        //   diferentes dos já usados.
+        
+        // Não esquecer: depois de acrescentar o código, fazer
+        //               Team | Commit
+        //               Team | Remote | Push
+        
+        // NOVO CÓDIGO SÓ ABAIXO DESTA LINHA
+        
+        
+    }
+}
